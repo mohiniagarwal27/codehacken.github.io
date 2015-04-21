bee3abcddd5d7df662baee513689a8acdab224bd
Codehacken <codehacken@gmail.com>
2015-04-20 22:16:43 -0400

    Adding angularjs and making the website sort of modular

---
+++ assets/js/main.js
@@ -0,0 +1,45 @@
+/**
+ * Created by ashwin on 4/20/15.
+ */
+/**
+ * Main AngularJS Web Application
+ */
+var app = angular.module('tutorialWebApp', [
+    'ngRoute'
+]);
+
+/**
+ * Configure the Routes
+ */
+app.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider
+        // Home
+        .when("/", {templateUrl: "assets/parts/home.html", controller: "PageCtrl"})
+        // Pages
+        .when("/research", {templateUrl: "assets/parts/research.html", controller: "PageCtrl"})
+        .when("/contact", {templateUrl: "assets/parts/contact.html", controller: "PageCtrl"})
+        .otherwise("/404", {templateUrl: "partials/404.html", controller: "PageCtrl"});
+
+}]);
+
+/**
+ * Controls the Blog
+ */
+app.controller('BlogCtrl', function (/* $scope, $location, $http */) {
+    console.log("Blog Controller reporting for duty.");
+});
+
+/**
+ * Controls all other Pages
+ */
+app.controller('PageCtrl', function (/* $scope, $location, $http */) {
+    // Activates the Carousel
+    $('.carousel').carousel({
+        interval: 5000
+    });
+
+    // Activates Tooltips for Social Links
+    $('.tooltip-social').tooltip({
+        selector: "a[data-toggle=tooltip]"
+    })
+});
