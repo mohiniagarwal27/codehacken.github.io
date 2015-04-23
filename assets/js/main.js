be0bc2493acc528d53901dde0764ca3381b67de2
Codehacken <codehacken@gmail.com>
2015-04-23 14:53:08 -0400

    Removing #

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,9 +11,9 @@ var app = angular.module('mywebsiteApp', [
 /**
  * Configure the Routes
  */
-app.config(['$routeProvider', function ($routeProvider, $locationProvider) {
+app.config(['$routeProvider', function ($routeProvider) {
 
-    $locationProvider.html5Mode(true);
+    //$locationProvider.html5Mode(true);
 
     $routeProvider
         // Home
